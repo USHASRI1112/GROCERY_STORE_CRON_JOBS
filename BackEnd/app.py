886471870f6bb00b4bbf395c19b2011e9d87ad97
USHASRI1112 <96167956+USHASRI1112@users.noreply.github.com>
2024-01-11 09:20:26 +0530

    Update app.py

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -510,4 +510,4 @@ db.init_app(app)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5050)
+    app.run(debug=True, port=5050,host='0.0.0.0')
